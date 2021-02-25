6854a3e296069249204a72077377030167c5b17d
zavyalovartem <zavyalovartem@users.noreply.github.com>
2021-02-25 10:53:28 +0500

    Testing heroku

---
+++ config.py
@@ -0,0 +1,2 @@
+TOKEN = '1694016582:AAGobMt3d3mruS_1sB7DFHqSm2UPt9zYKuU'
+URL = 'https://caseinprototype.herokuapp.com/'
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print(10)
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+import os
+import config
+
+updater = Updater(token=config.TOKEN, use_context=True)
+PORT = int(os.environ.get('PORT', '8443'))
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, я бот Атом, если ты новый сотрудник "
+                                                                    "корпорации как РосАтом, то я могу помочь тебе в "
+                                                                    "адаптации в нашем большом и дружном коллективе)")
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+
+updater.start_webhook(listen="0.0.0.0",
+                      port=int(PORT),
+                      url_path=config.TOKEN)
+updater.bot.set_webhook(config.URL + config.TOKEN)
+updater.idle()
